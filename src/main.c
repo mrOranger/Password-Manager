2523e8c94ab02ea93ba9676623e75d8a0afc9154
Edoardo Oranger <edo.oranger@gmail.com>
2021-01-31 15:38:55 +0100

    Git Initialized

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, const char** argv){
+    int age = 0;
+    printf("Hello World from Edoardo!");
+    scanf("%d", &age);
+    printf("You have %d years old", age);
+    return EXIT_SUCCESS;
+}
