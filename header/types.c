5dde5bda1f27a3ce8fe5852fdabee14878c25c93
Edoardo Oranger <edo.oranger@gmail.com>
2021-02-05 13:32:06 +0100

    UPDATE & ADD! Update file main.c (created menu). Add file types.c, boolean type created.

---
+++ header/types.c
@@ -0,0 +1,4 @@
+typedef enum boolean{
+    true = 0,
+    false = 1
+}bool;
--- src/main.c
+++ src/main.c
@@ -1,11 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "../header/types.c"
 
-int main(int argc, const char** argv){
-    int age = 0;
-    printf("Hello World from Edoardo!");
-    scanf("%d", &age);
-    printf("You have %d years old", age);
+static unsigned int print_menu();
+static bool check_choice(unsigned int choice);
+
+int main(int argc, const char **argv){
+    unsigned int choice = print_menu();
+    printf("You selected: %u", choice);
     return EXIT_SUCCESS;
+}
+
+static unsigned int print_menu(){
+    unsigned int choice = 0;
+    printf("\t ========== Password Manger ==========\n");
+    printf("\t 1. Generate random password.\n");
+    printf("\t 2. Show passwords.\n");
+    printf("\t 3. Remove a password.\n");
+    printf("\t 4. Modify a password.\n");
+    printf("\t 0. Exit.\n");
+    printf("\t Select a number: ");
+    scanf("%u", &choice);
+    if (check_choice(choice) == true){
+        return choice;
+    }else{
+        system("cls");
+        return print_menu();
+    }
+}
+
+static bool check_choice(unsigned int choice){
+    if ((choice <= 4) && (choice >= 0)){
+        return true;
+    }
+    return false;
 }
